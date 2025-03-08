9cabfadba0f3619edc6cd9c6919d882e675c13d9
Zh3ka21 <puzanov_zhenechk@ukr.net>
2025-03-08 22:54:39 +0200

    feat(docker): add a docker posible deployment method

--- temp.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-password = "123456789"
-stored_hash = "$2b$12$YpsSEk7MNuVFjZEaTg/yJuNEHnMp1nX8pRNcL0XcEDJy7fTMloPcm"  # The one from your DB
-
-# Verify if the password matches the stored hash
-print(pwd_context.verify(password, stored_hash))  # Should print: True
